23ffb4fb42d0b45eaf9609d322c52f8fd84fe4a6
gabriel.herrera@siteimpact.com <GalpalOneway77&&>
2020-04-05 10:36:32 -0400

    adding git ignore

--- product.py
+++ product.py
@@ -2,5 +2,9 @@ import time
 
 class Product():
 
-    def __init__(self):
+    def __init__(self, iterable=(), **kwargs):
+        self.__dict__.update(iterable, **kwargs)
         self.created_at = time.time()
+
+    def __iter__(self):
+        return iter([self.name, self.brand])
