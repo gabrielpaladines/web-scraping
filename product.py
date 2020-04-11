7e688427bb3bb65460358a4e63dd8af9937b3147
Gabriel Paladines–Primary –gabriel.paladines@gaphit.com <gabriel.paladines@gaphit.com>
2020-04-11 13:17:57 -0400

    Agregando recursividad de paginas y descripcion del descuento

--- product.py
+++ product.py
@@ -4,9 +4,17 @@ from datetime import date
 class Product():
 
     def __init__(self, iterable=(), **kwargs):
+        # Inicializamos el producto con el dictionario de datos
         self.__dict__.update(iterable, **kwargs)
+        # Inicializamos la descripcion del producto
+        self.descripcion = ''
+        # Colocamos la fecha de creacion
         self.created_at = date.today()
+        # Convertimos las categorias del producto a texto
         self.categories = ','.join([str(elem) for elem in self.category])
+        # Inicializamos los precios relacionados al productos
+        # el producto puede tener dos precios el original y el final
+        # estos dos difieren si existe un descuento a nivel de precios
         _price = self.price
         self.price_final = _price['final']
         self.price_original = _price['original'] if 'original' in _price \
@@ -15,4 +23,7 @@ class Product():
     def __iter__(self):
         return iter([self.name, self.brand, self.categories,
                      self.price_original, self.price_final,
-                     self.currency, self.status, self.created_at])
+                     self.currency, self.status,self.descripcion, self.created_at])
+
+    def set_description(self, description):
+        self.descripcion = description
--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 import json
 import csv
 from product import Product
-from pprint import pprint
 
 
 class FoodScraper():
@@ -13,39 +12,57 @@ class FoodScraper():
     def __init__(self, _url):
         self.url = _url
         self.data = []
+        self.max_pages = 2
+
 
     def __download_html(self, url):
-        page = requests.get(url)
-        html = page.content
+        html = None
+        page = requests.get(url, verify=True)
+
+        if page.status_code == 200:
+            html = page.content
+
         return html
 
 
     def buildProduct(self, food_item):
         food_dict = json.loads(food_item)
         product = Product(food_dict)
-        # pprint(vars(product))
         return product
 
 
     def scrape(self):
+        page_counter = 2
         print("Iniciando la busqueda de productos en oferta\n")
 
         # timer
         start_time = time.time()
-
         # Download HTML
         html = self.__download_html(self.url)
-        soup = BeautifulSoup(html, 'html.parser')
-        # print(soup.prettify())
 
-        items = soup.findAll("div", class_ = "grid-item")
-        print("Items encontrados: " + str(len(items)))
+        while html:
+            soup = BeautifulSoup(html, 'html.parser')
+            items = soup.findAll("div", class_ = "grid-item")
+            print("Items encontrados: " + str(len(items)))
 
-        for item in items:
-            food_data = item['data-json'] if 'data-json' in item.attrs else "no content"
-            product = self.buildProduct(food_data)
-            # Store the data
-            self.data.append(product)
+            for item in items:
+                food_data = item['data-json'] if 'data-json' in item.attrs else "no content"
+                product = self.buildProduct(food_data)
+                # Agregamos la descripcion del descuento
+                description = item.findAll("div", class_="offer-description")
+                if description:
+                    product.set_description(description[0].text)
+                # Almacenamos el producto
+                self.data.append(product)
+
+            if page_counter > self.max_pages:
+                break
+
+            # Download HTML
+            time.sleep(30)
+            html = self.__download_html(self.url + str(page_counter))
+            print(self.url + str(page_counter))
+            page_counter += 1
 
         # Show elapsed time
         end_time = time.time()
@@ -57,7 +74,7 @@ class FoodScraper():
         with open(file_name, 'w') as csv_file:
             wr = csv.writer(csv_file, delimiter=',')
             wr.writerow(["Nombre", "Marca", "Categorias", "Precio Original",
-                         "Precio Final", "Moneda", "Estado", "Fecha de captura"])
+                         "Precio Final", "Moneda", "Estado","Descripcion", "Fecha de captura"])
             for product in self.data:
                 wr.writerow(list(product))
 
